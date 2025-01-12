1329626cd109ccaab8c4fb023e5bc773f1784978
robertmacwan17 <robert.macwan@groovyweb.co>
2025-01-12 15:08:43 +0530

    removed TTS feature

--- src/components/GeneratedContentToolbar/GeneratedContentToolbar.tsx
+++ src/components/GeneratedContentToolbar/GeneratedContentToolbar.tsx
@@ -1,17 +1,17 @@
 "use client"
 import { Box, Flex, For, HStack, IconButton, Text, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
-import { HiSpeakerWave } from 'react-icons/hi2'
+// import { HiSpeakerWave } from 'react-icons/hi2'
 import { Tooltip } from '../ui/tooltip'
 import { IoCopy, } from 'react-icons/io5'
 import { BsFillSave2Fill } from 'react-icons/bs'
 import { copyToClipBoard } from '../../app/utils/commonFunctions'
-import { FaPause } from 'react-icons/fa'
+// import { FaPause } from 'react-icons/fa'
 import { SaveDraftModal } from '../SaveDraftModal/SaveDraftModal'
 import { useSelector } from 'react-redux'
 import type { RootState } from '@blogiq/store/store'
 import { isEmpty } from 'lodash'
-import useTextToSpeech from '@blogiq/hooks/useTextToSpeech'
+// import useTextToSpeech from '@blogiq/hooks/useTextToSpeech'
 
 interface GeneratedContentToolbarProps {
     generatedContent: string;
@@ -19,17 +19,17 @@ interface GeneratedContentToolbarProps {
 
 const GeneratedContentToolbar: React.FC<GeneratedContentToolbarProps> = ({ generatedContent }) => {
     const [openDraftModal, setOpenDraftModal] = useState(false);
-    const [ttsStatus, setTTSStatus] = useState("idle");
+    // const [ttsStatus, setTTSStatus] = useState("idle");
     const userData = useSelector((state: RootState) => state.app.userData);
-    const { speak } = useTextToSpeech();
+    // const { speak } = useTextToSpeech();
     const toolbarOptions = [
-        {
-            id: 1,
-            label: ttsStatus === "playing" ? "Pause" : "Read Aloud",
-            icon: ttsStatus === "playing" ? <FaPause color='white' /> : <HiSpeakerWave color='white' />,
-            action: handleSpeechAction,
-            active: true,
-        },
+        // {
+        //     id: 1,
+        //     label: ttsStatus === "playing" ? "Pause" : "Read Aloud",
+        //     icon: ttsStatus === "playing" ? <FaPause color='white' /> : <HiSpeakerWave color='white' />,
+        //     action: handleSpeechAction,
+        //     active: true,
+        // },
         {
             id: 2,
             label: 'Copy',
@@ -46,22 +46,22 @@ const GeneratedContentToolbar: React.FC<GeneratedContentToolbarProps> = ({ gener
         }
     ]
 
-    function handleSpeechAction() {
-        if (ttsStatus === "playing") {
-            window.responsiveVoice.pause();
-            setTTSStatus("paused");
-        } else if (ttsStatus === "paused") {
-            window.responsiveVoice.resume();
-            setTTSStatus("playing");
-        } else {
-            speak(generatedContent, {
-                voice: "UK English Male",
-                rate: 1.1,
-                onstart: () => setTTSStatus("playing"),
-                onend: () => setTTSStatus("idle"),
-            });
-        }
-    };
+    // function handleSpeechAction() {
+    //     if (ttsStatus === "playing") {
+    //         window.responsiveVoice.pause();
+    //         setTTSStatus("paused");
+    //     } else if (ttsStatus === "paused") {
+    //         window.responsiveVoice.resume();
+    //         setTTSStatus("playing");
+    //     } else {
+    //         speak(generatedContent, {
+    //             voice: "UK English Male",
+    //             rate: 1.1,
+    //             onstart: () => setTTSStatus("playing"),
+    //             onend: () => setTTSStatus("idle"),
+    //         });
+    //     }
+    // };
 
     return (
         <Flex position="relative" top="-2px" borderRadius="0 0 15px 15px" background="#181818" justifyContent="space-between" alignItems="center">
